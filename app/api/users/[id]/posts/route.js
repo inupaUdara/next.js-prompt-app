813331e05f0933df408a5114a0f5c729b76efa55
Inupa_Udara <127815279+inupaUdara@users.noreply.github.com>
2024-06-24 19:39:43 +0530

    Add create prompt, view prompt and user profile

---
+++ app/api/users/[id]/posts/route.js
@@ -0,0 +1,19 @@
+import { connectToDB } from "@utils/database";
+import Prompt from "@models/prompt";
+
+export const GET = async (request, { params }) => {
+    try {
+        await connectToDB();
+
+        const prompt = await Prompt.find({
+            creator: params.id
+        }).populate('creator');
+
+        return new Response(JSON.stringify(prompt), {
+            status: 200,
+        });
+        
+    } catch (error) {
+        return new Response("Failed to fetch your promptes", {status: 500})
+    }
+}
